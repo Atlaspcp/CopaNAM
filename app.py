1c8773b2ad0e42cc153c75f01c29350defeb0f6c
Atlaspcp <joel.valenzuela@trewhelaschool.cl>
2026-04-16 08:22:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 import streamlit as st
 import random
 
-st.title("🎯 Juego: Adivina el Número")
+st.title("Juego: Adivina el Número")
 
-# Generamos el número secreto (Ojo: se reinicia en cada clic si no se usa algo llamado session_state, 
-# pero para nivel inicial, que sea un número fijo o aleatorio por ejecución está bien para aprender)
-secreto = 7 
+numero = 7 
 
 intento = st.number_input("Introduce un número del 1 al 10", min_value=1, max_value=10)
 
 if st.button("¿Habré acertado?"):
-    if intento == secreto:
-        st.balloons() # ¡Efecto visual de celebración!
+    if intento == numero:
+        st.balloons()
         st.success("¡Felicidades! Ganaste.")
-    elif intento < secreto:
+    elif intento < numero:
         st.warning("Demasiado bajo...")
     else:
         st.warning("Demasiado alto...")
